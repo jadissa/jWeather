7cdb4651a1ed36a0e004bfcbd77f3d5af52fb885
jadissa <5577795+jadissa@users.noreply.github.com>
2025-12-18 06:13:05 -0600

    documentation

--- tests/Feature/GenerateWeatherImageTest.php
+++ tests/Feature/GenerateWeatherImageTest.php
@@ -11,15 +11,33 @@ use Tests\TestCase;
 class GenerateWeatherImageTest extends TestCase
 {
     /**
-     * Test that the GenerateWeatherImage command runs successfully.
+     * Test that the GenerateWeatherImage command runs successfully for the weather
      *
      * @return void
      */
-    public function test_generate_weather_image_command_is_successful(): void
+    public function test_generate_weather_image_is_successful(): void
     {
         
         // Define the expected output message from the command
-        $expectedOutput = 'Image generated successfully at public/images/out.png';
+        $expectedOutput = 'Image generated successfully at public/images/weather.png';
+
+        // Call the artisan command using the fluent API
+        $this->artisan(GenerateWeatherImage::class)
+             ->expectsOutput($expectedOutput)
+             ->assertSuccessful();
+
+    }
+
+    /**
+     * Test that the GenerateWeatherImage command runs successfully for the clock
+     *
+     * @return void
+     */
+    public function test_generate_clock_image_is_successful(): void
+    {
+        
+        // Define the expected output message from the command
+        $expectedOutput = 'Image generated successfully at public/images/clock.png';
 
         // Call the artisan command using the fluent API
         $this->artisan(GenerateWeatherImage::class)
