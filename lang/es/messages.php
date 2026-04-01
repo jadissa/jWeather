90d1eb9db5b0fb9590f9acf3d211b899cb98442a
jadissa <5577795+jadissa@users.noreply.github.com>
2026-03-31 23:36:52 -0500

    Spanish translation

---
+++ lang/es/messages.php
@@ -0,0 +1,16 @@
+    <?php
+
+    return [
+        'feelslike'         => 'F Como',
+        'cloud_coverage'    => 'Cobertura nubosa',
+        'decreasing'        => 'Decreciente',
+        'increasing'        => 'Creciente',
+        'gusts'             => 'Ráfagas',
+        'humidity'          => 'Humedad',
+        'cloudy'            => 'Nublada',
+        'chance_of_rain'    => 'Probabilidad de lluvia',
+        'chance_of_snow'    => 'Probabilidad de nieve',
+        'wind'              => 'Viento',
+        'alert'             => 'Alerta',
+        'generated_at'      => 'Generado el',
+    ];
